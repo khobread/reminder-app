4436369df3b8e029bfca26301512afef0f6cfc75
Kevin <>
2021-05-18 11:42:45 -0700

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3> Reminders </h3>
+      <br />
+      <form>
+        <label htmlFor="id">ID</label>
+        <input name="id" placeholder="ID" />
+        <label htmlFor="reminder">Reminder</label>
+        <input name="reminder" placeholder="Reminder" />
+        <label htmlFor="time">Time</label>
+        <input name="time" placeholder="Time" />
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 }
